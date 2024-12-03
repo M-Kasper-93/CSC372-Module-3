3c9dd9500baedbb8d790f7fcad0354c3db1eae98
M-Kasper-93 <megan.kasper@csuglobal.edu>
2024-12-02 21:29:32 -0700

    option 1 complete

--- userInterface/src/userInterface/GUIClass.java
+++ userInterface/src/userInterface/GUIClass.java
@@ -10,7 +10,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.GridPane;
+import java.time.LocalDateTime;
+/*import javafx.scene.layout.GridPane;*/
 
 public class GUIClass {
 	public class GUIClass extends Application {
@@ -19,10 +20,35 @@ public class GUIClass {
 		@Override
 		public void start(Stage stage) {
 			Scene scene = null;
-			GridPane gridPane = null;
+			TextField dateTime = null;
+			
+			MenuItem option1 = new MenuItem("Display date and time");
+			MenuItem option2 = new MenuItem("Generate .txt file");
+			MenuItem option3 = new MenuItem("Change frame color");
+			MenuItem option4 = new MenuItem("Exit program");
+			
+			//First option displays date and time
+			option1.setOnAction(EventHandlerActionEvent) new EventHandlerActionEvent() {
+				@Override
+				public void handle(ActionEvent event) {
+					dateTime = new TextField();
+					LocalDateTime currentDateTime = LocalDateTime.now();
+					dateTime.setText("Current date and time: " + currentDateTime);
+					dateTime.setEditable(false);
+				}
+			}
+			
+			//Second option writes text box contents to a text file named "log.txt"
+			option2.setOnAction(EventHandlerActionEvent) new EventHandlerActionEvent() {
+				@Override
+				public void handle(ActionEvent event) {
+					
+				}
+			}
+			/*GridPane gridPane = null;
 			
 			gridPane = new GridPane();
-			scene = new Scene(gridPane);
+			scene = new Scene(gridPane);*/
 			
 			
 			
